eb39cdf5fd5f973e0e1fee66de2a75851ecd548c
Glory Ogbondah <geethatcher@gmail.com>
2023-02-24 09:20:54 +0000

    text

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,11 +1,29 @@
 #!/usr/bin/python3
-def text_indentation(text):
-    if isinstance(text, str) is False:
-        raise TypeError("text must be a string")                
-    delim = ":.?"                    
-    for x in delim:         
-        text = text.replace(x, "{}\n\n".format(x))               
-    text = text.replace('\\', '')                                        
-    text = "\n".join([line.strip() for line in text.split('\n')])                             
-    print(text, end="")
+"""Defines a text-indentation function."""
 
+
+def text_indentation(text):
+    """Print text with two new lines after each '.', '?', and ':'.
+
+    Args:
+        text (string): The text to print.    
+    Raises:
+         TypeError: If text is not a string.       
+    """                                    
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+                             
+    c = 0
+    while c < len(text) and text[c] == ' ': 
+        c += 1
+                                       
+    while c < len(text):
+        print(text[c], end="")                
+        if text[c] == "\n" or text[c] in ".?:":                  
+            if text[c] in ".?:":                               
+                print("\n")                
+            c += 1
+            while c < len(text) and text[c] == ' ':  
+                c += 1                                       
+            continue              
+        c += 1
